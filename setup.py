46060923c5e6d4fa9a81674f6f0a973374c265ed
Ryan Hamilton <ryangilberthamilton@gmail.com>
2024-01-25 13:30:00 -0500

    Add setup.py and ykwmp/__init__.py files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ykwmp',
+    version='0.0.0',
+    description="Yukon Wetland Mapping Project",
+    author='Ryan Hamilton',
+    author_email='ryan.hamilton@ec.gc.ca',
+    packages=find_packages(include=['ykwmp', 'ykwmp.*']),
+)
