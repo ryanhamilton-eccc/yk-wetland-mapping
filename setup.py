7a6706d479d2574e3b0d4ccf3c5d78fe9c308f4a
Ryan Hamilton <ryangilberthamilton@gmail.com>
2024-02-01 11:19:43 -0500

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ykwmp',
-    version='0.0.0',
+    version='0.1.0',
     description="Yukon Wetland Mapping Project",
     author='Ryan Hamilton',
     author_email='ryan.hamilton@ec.gc.ca',
