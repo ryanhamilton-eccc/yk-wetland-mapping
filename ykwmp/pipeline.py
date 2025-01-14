4078a4db41635cc9a39de23704bb3db864c04553
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2025-01-14 16:09:01 -0500

    add pipeline steps

--- ykwmp/pipeline.py
+++ ykwmp/pipeline.py
@@ -1,46 +1,97 @@
-def run_step(task_function, *args, **kwargs):
-    """
-    Template function for executing a step in the pipeline.
-    - Loads assets
-    - Calls the provided task function with the necessary arguments
-    - Cleans up the environment
-    """
-    # Load asset (if applicable)
-    print("Loading asset...")
-    # You can define logic here to load any assets specific to each step
+from pathlib import Path
 
-    # Execute the task
-    print("Running task...")
-    result = task_function(*args, **kwargs)
+import ee
+
+# -- internal imports
+from ykwmp.eesys import create_asset_folder, asset_exists, list_assets_in_path
+from ykwmp.injest import batch_ingest_shapefiles, ingest_shapefile_to_asset
+from ykwmp.image_processing import process_s1_images, process_s2_images
+from ykwmp.image_utils import batch_extract
+from ykwmp.classification import batch_model_and_assesment
+
+
+def setup_workspace_step(workspace: str, region_path: str | Path) -> None:
+
+    # -- create workspace
+    # -- injest region shapefile
+    if isinstance(region_path, str):
+        region_path = Path(region_path)
     
-    # Clean up processing environment (e.g., clearing variables, freeing memory)
-    print("Cleaning up...")
-    # Include any necessary cleanup steps (e.g., `ee.data.clear()`)
+    asset_id = f"{workspace}/{region_path.name.split('.')[0]}_region"
+    if asset_exists(asset_id):
+        print("Workspace already setup..")
+        return
+    
+    create_asset_folder(workspace)
+    
+    ingest_shapefile_to_asset(
+        shapefile_path=region_path,
+        asset_id=asset_id
+    )
 
-    return result
+
+def ingest_trainval_step(trainval_root: str | Path, ee_workspace: str) -> None:
+    # -- run injestor
+    batch_ingest_shapefiles(trainval_root, ee_workspace)
 
 
-def feature_extraction_step(aoi, date_range):
-    def feature_extraction_task(aoi, date_range):
-        # Feature extraction logic here
-        # For example: extract features, process images, etc.
-        return features  # Example return value
+def batch_feature_extraction_step(workspace: str):
+    # -- get the meta data
+    region_id = list_assets_in_path(workspace, "_region$")
+    features = list_assets_in_path(workspace, "_raw$")
 
-    # Use the template function to run the feature extraction step
-    return run_step(feature_extraction_task, aoi, date_range)
+    # -- extract the ids
+    region_id = region_id[0].get('id')
+    features = list(map(lambda x: x.get('id'), features))
 
-def model_training_step(features, class_property, input_properties):
-    def model_training_task(features, class_property, input_properties):
-        # Model training logic here
-        model = randomforest(features, class_property, input_properties)
-        return model
+    # consturct region
+    region = ee.FeatureCollection(region_id).geometry()
+    # -- s1 inputs
+    s1 = process_s1_images(region, dates=("2019-06-20", "2019-09-21"))
+    # -- s2 inputs
+    s2 = process_s2_images(region)
 
-    return run_step(model_training_task, features, class_property, input_properties)
+    # create the stack
+    stack = ee.Image.cat(s1, s2) 
+    
+    # run batch extract tool
+    batch_extract(stack=stack, assets=features)
 
-def accuracy_assessment_step(model, validation_features, actual_property, predicted_property):
-    def accuracy_assessment_task(model, validation_features, actual_property, predicted_property):
-        # Accuracy assessment logic here
-        metrics = compute_accuracy_metrics(model, validation_features, actual_property, predicted_property)
-        return metrics
+    ee.Reset()
+    return
 
-    return run_step(accuracy_assessment_task, model, validation_features, actual_property, predicted_property)
+def batch_model_and_assessment_step(workspace):
+    if not ee.data._credentials:
+        ee.Initialize(project="yk-wetland-mapping")
+        print("Re init api")
+    inputs = list_assets_in_path(workspace, pattern="_feature$")
+    inputs = list(map(lambda x: x.get('id'), inputs))
+    batch_model_and_assesment(features=inputs, folder_name="165_metrics")
+    ee.Reset()
+    return
+
+
+def batch_predict(workspace):
+    pass
+
+
+if __name__ == '__main__':
+    ee.Initialize(project="yk-wetland-mapping")
+    setup_workspace_step(
+        workspace="projects/yk-wetland-mapping/assets/test-workspace",
+        region_path=r"D:\Yukon-Wetland-Mapping-Data\YK_Seperated_AOI\165.shp"
+    )
+
+    # -- step 2: Injest Training and Validation to ee file system
+    ingest_trainval_step(
+        trainval_root=r"D:\Yukon-Wetland-Mapping-Data\YK_GeeReady\165",
+        ee_workspace="projects/yk-wetland-mapping/assets/test-workspace"
+    )
+
+    # -- step 3: feature extraction
+    batch_feature_extraction_step("projects/yk-wetland-mapping/assets/test-workspace")
+
+    # -- step 4: modeling and assessment
+    batch_model_and_assessment_step("projects/yk-wetland-mapping/assets/test-workspace")
+
+    # -- step 5: batch prediction
